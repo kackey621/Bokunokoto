f745965b037eb105fd0844914dd3588d0a216db9
kackey621 <a.kusama@icloud.com>
2026-05-10 10:37:02 +0900

    refactor: update sidebar toggle logic and refine hamburger menu animation and styling

--- app/javascript/controllers/sidebar_controller.js
+++ app/javascript/controllers/sidebar_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["line1", "line2", "line3"]
+  static targets = ["line1", "line2", "line3", "sidebar"]
 
   connect() {
     this.isOpen = false
@@ -17,21 +17,21 @@ export default class extends Controller {
     
     // Toggle sidebar translation
     if (this.isOpen) {
-      this.element.classList.remove("-translate-x-full")
+      this.sidebarTarget.classList.remove("-translate-x-full")
     } else {
-      this.element.classList.add("-translate-x-full")
+      this.sidebarTarget.classList.add("-translate-x-full")
     }
 
     // Toggle hamburger icon animation
     if (this.hasLine1Target && this.hasLine2Target && this.hasLine3Target) {
       if (this.isOpen) {
-        this.line1Target.classList.add("rotate-45", "translate-y-1.5")
+        this.line1Target.classList.add("rotate-45", "translate-y-2")
         this.line2Target.classList.add("opacity-0")
-        this.line3Target.classList.add("-rotate-45", "-translate-y-1.5")
+        this.line3Target.classList.add("-rotate-45", "-translate-y-2")
       } else {
-        this.line1Target.classList.remove("rotate-45", "translate-y-1.5")
+        this.line1Target.classList.remove("rotate-45", "translate-y-2")
         this.line2Target.classList.remove("opacity-0")
-        this.line3Target.classList.remove("-rotate-45", "-translate-y-1.5")
+        this.line3Target.classList.remove("-rotate-45", "-translate-y-2")
       }
     }
   }
